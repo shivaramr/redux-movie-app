5aaaf3982858420859b7d335d16d5b8f2687c4ac
shivaramr <85446156+shivaramr@users.noreply.github.com>
2023-05-22 09:27:46 +0530

    pagenotfound code formating

--- src/components/PageNotFound/PageNotFound.js
+++ src/components/PageNotFound/PageNotFound.js
@@ -1,13 +1,13 @@
-import React, { memo } from 'react'
-import pnf from '../../images/pnf.jpg'
-import './PageNotFound.scss'
+import React, { memo } from "react";
+import pnf from "../../images/pnf.jpg";
+import "./PageNotFound.scss";
 
 const PageNotFound = memo(() => {
   return (
-    <div className='pnf-page'>
-      <img src={pnf} alt="Page Not Found..." className='pnf-img'/>
+    <div className="pnf-page">
+      <img src={pnf} alt="Page Not Found..." className="pnf-img" />
     </div>
-  )
-})
+  );
+});
 
-export default PageNotFound
+export default PageNotFound;
